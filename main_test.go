1bbb70c7373a791760f85acfb81772438a9b68ca
iccy <iccy.fun@outlook.com>
2020-12-03 23:48:23 +0800

    test
    Added Test_dateStrToNum(). This function tests whether dateStrToNum() works properly

--- main_test.go
+++ main_test.go
@@ -5,3 +5,26 @@
  */
 
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_dateStrToNum(t *testing.T) {
+	str := []struct {
+		date string
+		num  []int
+	}{
+		{"1949.10.1", []int{1949, 10, 1}},
+		{"2000.4.15", []int{2000, 4, 15}},
+		{"2000.09.16", []int{2000, 9, 16}},
+		{"2020.12.3", []int{2020, 12, 3}},
+	}
+
+	for _, v := range str {
+		if numArray, _ := dateStrToNum(v.date); !reflect.DeepEqual(numArray, v.num) {
+			t.Log(numArray, " != ", v.num)
+		}
+	}
+}
