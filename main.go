fa972d9a06adaf0c4cf52744e7892d62bd545d38
iccy <iccy.fun@outlook.com>
2020-12-03 23:07:58 +0800

    feat
    Added dateStrToNum(), This function converts the date string into the parameters required for Zeller's formula

--- main.go
+++ main.go
@@ -7,13 +7,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
 func main() {
+	var err error
+
 	if len(os.Args) != 2 || os.Args[1] == "-h" || os.Args[1] == "--help" {
 		fmt.Println("Please enter a date in this format -- \033[31myyyy.mm.dd\033[0m\n$ Zeller 1949.10.1")
 		return
@@ -21,8 +26,43 @@ func main() {
 
 	log.SetFlags(log.LstdFlags ^ log.Ldate ^ log.Ltime)
 
-	if _, err := time.Parse("2006.1.2", os.Args[1]); err != nil {
+	if _, err = time.Parse("2006.1.2", os.Args[1]); err != nil {
 		log.Fatalln(err)
 	}
 
+	var numArray []int
+	if numArray, err = dateStrToNum(os.Args[1]); err != nil {
+		log.Fatalln(err)
+	}
+	_ = numArray
+}
+
+/*
+ * @Description: Cut the date string and turn it into a number
+ * @Param: string
+ * @Return: []int, error
+ * @Author: iccy
+ * @Date: 2020/12/3
+ */
+func dateStrToNum(data string) (num []int, err error) {
+	strArray := strings.Split(data, ".")
+	if len(strArray) != 3 {
+		return []int{}, errors.New("incorrect parameter format")
+	}
+
+	for k, _ := range strArray {
+		num[k], err = strconv.Atoi(strArray[k])
+		check(&err)
+	}
+	if err != nil {
+		return []int{}, errors.New("parameter error")
+	}
+
+	return num, nil
+}
+
+func check(err *error) {
+	if *err != nil {
+		log.Println(*err)
+	}
 }
